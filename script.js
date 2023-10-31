0dc6d04011b30aecba543d51aa9bfa8c94b3a421
Mohamad Ali Calanda <135398263+MohamadAli20@users.noreply.github.com>
2023-10-31 17:46:56 +0800

    commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // for notice
-alert("This web application is still in progress. Sila Janna at Sharmine ang tatapos nito hehe\n - Ali");
+alert("This web application is still in progress. \nSila Janna at Sharmine ang tatapos nito hehe\n - Ali");
 
 // get the value of username and password and display it
 const btnLogin = document.getElementById("login-btn");
@@ -11,17 +11,31 @@ btnLogin.addEventListener("click", function(){
 });
 
 // show and hide password using mouseover and mouseout
-const viewIcon = document.getElementById("view-icon");
-
-viewIcon.addEventListener("mouseover", function(){
+// for login form
+const loginView = document.getElementById('login-view');
+loginView.addEventListener("mouseover", function(){       
+    // document.getElementById('login-input').type = "text";
     document.forms['login-form']['password'].type = "text";
 });
-viewIcon.addEventListener("mouseout", function(){
+loginView.addEventListener("mouseout", function(){
+    // document.getElementById('login-input').type = "password";
     document.forms['login-form']['password'].type = "password";
 });
 
+// forgot password
+const forgotPass = document.getElementById('forgot-password');
+forgotPass.addEventListener("mouseover", function(){
+    forgotPass.style.textDecoration = "underline";
+});
+forgotPass.addEventListener("mouseout", function(){
+    forgotPass.style.textDecoration = "none";
+});
 
-
+// for registratin form
+// check if both passwords are matches and less than 6 characters
+// const regPass = document.getElementById('reg-pass').value;
+// const regConfirmPass = doocument.getElementById('reg-confirmpass').value;
+// const warningMessage = document.getElementById('warning-message');
 
 
 
